ef9fda5dc5d0696a6c8d10e9aed581dddda9ea37
CrucialWalnuts <johnsmithok6224@gmail.com>
2023-10-05 11:40:54 -0700

    Added Particles

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -9,23 +9,34 @@ public class GameController : MonoBehaviour
     public int totalSpheres = 5;
     private int collectedSpheres = 0;
     // Start is called before the first frame update
+    [SerializeField] ParticleSystem collectParticles;
+    [SerializeField] ParticleSystem successParticles;
     void Start()
     {
         winText = GameObject.FindWithTag("WinMessage").GetComponent<Text>();
         winText.text = String.Format("You have collected {0}/{1} spheres", collectedSpheres, totalSpheres);
     }
-    
+
     public void SphereCollected()
     {
         //Debug.Log("Sphere collected");
-       
+
         collectedSpheres++;
+
         winText.text = String.Format("You have collected {0}/{1} spheres", collectedSpheres, totalSpheres);
         if (collectedSpheres == totalSpheres)
         {
-             winText.text = String.Format("Congratulations! You have collected all spheres");
+            winText.text = String.Format("Congratulations! You have collected all spheres");
+            successParticles.Play();
+
+
+        }
+        else
+        {
+            collectParticles.Play();
         }
     }
+
 
     // Update is called once per frame
     void Update()
